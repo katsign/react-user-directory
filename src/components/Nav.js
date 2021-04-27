a35c583622284be1837211cbae8504a1e5fb9ef0
Kat Buchanan <katrbuch@gmail.com>
2021-04-27 16:56:21 -0500

    nav component

---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+
+const Nav = () => {
+  return (
+    <>
+      <nav className="navbar navbar-light justify-content-between">
+        <a className="navbar-brand" href="/">User Directory</a>
+        <a className="navbar-brand" href="https://github.com/katsign/react-user-directory"><small>Repo</small></a>
+      </nav>
+    </>
+  );
+};
+
+export default Nav;
