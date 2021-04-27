63a91948640804475519511fe678faeb5e8f8059
Kat Buchanan <katrbuch@gmail.com>
2021-04-27 18:03:02 -0500

    path fix

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,7 +3,7 @@ const Nav = () => {
   return (
     <>
       <nav className="navbar navbar-light justify-content-between">
-        <a className="navbar-brand" href="/">User Directory</a>
+        <a className="navbar-brand" href="/react-user-directory/">User Directory</a>
         <a className="navbar-brand" href="https://github.com/katsign/react-user-directory"><small>Repo</small></a>
       </nav>
     </>
