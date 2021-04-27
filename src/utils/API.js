a87c882ae9dc0845aaa18a954b24657af8ec3bf7
Kat Buchanan <katrbuch@gmail.com>
2021-04-27 17:29:44 -0500

    increased call limit

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const getUsers = {
   getUsers: function () {
-    return axios.get("https://randomuser.me/api/?results=40&nat=us");
+    return axios.get("https://randomuser.me/api/?results=80&nat=us");
   },
 };
 
